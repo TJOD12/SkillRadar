5b794a7136cc990993f2c4a7227812f076da3fd6
Tom O'Dwyer <tomodwyer11@gmail.com>
2026-09-21 23:41:07 +0100

    Collect all skills in occurrence

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,6 @@
-import { useEffect, useState, type ChangeEvent } from 'react'
-import { getJobs, triggerAdminScrape, filterCities, filterSkills } from './services/api'
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Metrics from "./pages/Metrics";
 import Jobs from './pages/Jobs'
-import JobCard from './components/JobCard'
-import LoadingCard from './components/LoadingCard'
-import type { Job } from './types/job'
 
 import './App.css'
 
--- frontend/src/pages/Metrics.tsx
+++ frontend/src/pages/Metrics.tsx
@@ -1,30 +1,49 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, type SetStateAction } from 'react'
 import { getJobs } from '../services/api'
 import type { Job } from '../types/job'
 
 import '../App.css'
 
 function Metrics() {
-    //const [jobs, setJobs] = useState<Job[]>([])
+    const [jobs, setJobs] = useState<Job[]>([]);
+    const [skills, setSkills] = useState<string[]>([]);
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState<string | null>(null)
 
-    // useEffect(() => {
-    //     async function loadJobs() {
-    //         try {
-    //            const data = await getJobs();
-    //            setJobs(data);
-    //            parseMetrics(jobs);
-    //         } catch (error) {
-    //             console.log(error)
-    //         }
-    //     }
-    // })
+    useEffect(() => {
+        async function loadJobs() {
+            try {
+               const data = await getJobs();
+               setJobs(data);
+               parseMetrics(data);
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        loadJobs()
+    }, [])
 
-    // function parseMetrics(jobs: Job[]) {
-        
-    // }
+    function parseMetrics(jobs: Job[]) {
+        let skillsMetricList: string[] = [];
+        console.log("parsing")
+        try {
+            for (let job of jobs) {
+                console.log(job.jobSkills)
+                for (let skill of job.jobSkills) {
+                    skillsMetricList.push(skill)
+                }
+            }
+            setSkills(skillsMetricList);
+        } catch (e) {
+            console.log(e)
+        }
+    }
 
     return (
-        <div>Hello</div>
+        <main>
+            Hello
+            { skills }
+        </main>
     )
 }
 
